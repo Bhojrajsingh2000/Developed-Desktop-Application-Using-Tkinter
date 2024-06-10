9266c8bb62ac3e57a6e71edc988cc60af0e60cdd
BHOJRAJ SINGH <129201597+Bhojrajsingh2000@users.noreply.github.com>
2024-06-10 17:28:02 +0530

    Add files via upload

---
+++ Paint Application/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+def setup(self):
+        self.old_x = None
+        self.old_y = None
+        self.line_width = self.choose_size_button.get()
+        self.color = self.DEFAULT_COLOR
+        self.eraser_on = False
+        self.active_button = self.pen_button
+        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<ButtonRelease-1>', self.reset)
---
+++ notepad/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+# root.iconbitmap('notepad.ico')
